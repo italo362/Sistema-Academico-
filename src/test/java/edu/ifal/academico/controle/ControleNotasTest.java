1b7f1b02d839dda4d228d3c19170574732fa95c8
italo362 <italo362@outlook.com>
2019-09-09 09:03:10 -0300

    implementação dos metodos de inicializacao na classe ControleNotasTest

--- src/test/java/edu/ifal/academico/controle/ControleNotasTest.java
+++ src/test/java/edu/ifal/academico/controle/ControleNotasTest.java
@@ -1,5 +1,35 @@
 package edu.ifal.academico.controle;
 
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ifal.academico.modelo.Estudante;
+
 public class ControleNotasTest {
 
+	private Estudante estudante;
+	private Estudante estudante1;
+	private Estudante estudante2;
+	
+	private  ControleNotas ControleNotas;
+	private CriadorDeEstudante criadorDeEstudante;
+		
+	
+	@Before
+	public void incializacao() {
+		 
+		estudante1 = new Estudante();
+		estudante2 = new Estudante();
+		estudante = new Estudante();
+		
+		ControleNotas ControleNotas;
+
+		CriadorDeEstudante criadorDeEstudante;
+
+	}
+	
+	
 }
