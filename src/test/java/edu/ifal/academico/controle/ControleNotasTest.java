de2d6c24e08969ab3f125b1e969ab9a6684b2e5c
italo362 <italo362@outlook.com>
2019-09-09 10:06:30 -0300

    implementação de tres  metodos de teste na classe ControleNotasTest

--- src/test/java/edu/ifal/academico/controle/ControleNotasTest.java
+++ src/test/java/edu/ifal/academico/controle/ControleNotasTest.java
@@ -1,11 +1,11 @@
 package edu.ifal.academico.controle;
 
-
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.ifal.academico.modelo.Endereco;
 import edu.ifal.academico.modelo.Estudante;
 
 public class ControleNotasTest {
@@ -14,9 +14,9 @@ public class ControleNotasTest {
 	private Estudante estudante1;
 	private Estudante estudante2;
 	
-	private  ControleNotas ControleNotas;
+	private  Estudante ControleNotas;
 	private CriadorDeEstudante criadorDeEstudante;
-		
+ Endereco endereco;
 	
 	@Before
 	public void incializacao() {
@@ -28,8 +28,42 @@ public class ControleNotasTest {
 		ControleNotas ControleNotas;
 
 		CriadorDeEstudante criadorDeEstudante;
+		 Endereco endereco;
 
 	}
 	
+	@Test
+	public void TestDeveFuncionarSeANotaForMaiorQue6() {
+		
 	
+		ControleNotas.getMedia();
+	
+		double valoresperado = 5;
+		double valorObtido = 6;
+		
+		assertEquals(valorObtido, valoresperado,0.001);
+
+	}
+	
+	@Test
+	public void TestNaoDeveFuncionarSeANotaFor6() {
+		ControleNotas.getMedia();
+		
+		double valoresperado = 7;
+		double valorObtido = 6;
+		
+		assertEquals(valorObtido, valoresperado,0.001);
+
+	}
+	
+	@Test
+	public void TestDeveFuncionarSeANotaForMenorQue6() {
+		ControleNotas.getMedia();
+		
+		double valoresperado = 7;
+		double valorObtido = 5;
+		
+		assertEquals(valorObtido, valoresperado,0.001);
+
+	}
 }
